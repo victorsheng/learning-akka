f38074c3ae08a8a53975312940c2a2966702b414
victor <920443721@qq.com>
2018-11-14 07:42:15 +0800

    读取配置文件,否则没有监听端口

--- ch2/akkademy-db-scala/src/main/scala/com/akkademy/AkkademyDb.scala
+++ ch2/akkademy-db-scala/src/main/scala/com/akkademy/AkkademyDb.scala
@@ -1,8 +1,10 @@
 package com.akkademy
 
-import akka.actor.{Props, ActorSystem, Status, Actor}
+import akka.actor.{Actor, ActorSystem, Props, Status}
 import akka.event.Logging
-import com.akkademy.messages.{KeyNotFoundException, GetRequest, SetRequest}
+import com.akkademy.messages.{GetRequest, KeyNotFoundException, SetRequest}
+import com.typesafe.config.ConfigFactory
+
 import scala.collection.mutable.HashMap
 
 class AkkademyDb extends Actor {
@@ -13,12 +15,15 @@ class AkkademyDb extends Actor {
     case SetRequest(key, value) =>
       log.info("received SetRequest - key: {} value: {}", key, value)
       map.put(key, value)
+      //返回成功
       sender() ! Status.Success
     case GetRequest(key) =>
       log.info("received GetRequest - key: {}", key)
       val response: Option[Object] = map.get(key)
-      response match{
+      response match {
+        //存在值
         case Some(x) => sender() ! x
+        //不存在值
         case None => sender() ! Status.Failure(new KeyNotFoundException(key))
       }
     case o => Status.Failure(new ClassNotFoundException)
@@ -26,6 +31,8 @@ class AkkademyDb extends Actor {
 }
 
 object Main extends App {
-  val system = ActorSystem("akkademy")
+  //之前的代码没有读取配置文件
+  val system = ActorSystem.create("akkademy", ConfigFactory.load("application.conf"))
+  //将自己变成的Actor子类,通过Props类传入系统
   val helloActor = system.actorOf(Props[AkkademyDb], name = "akkademy-db")
 }
